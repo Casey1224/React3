93b86dbe61a873aa5e65bbf289c6342fba3475e4
Casey Collins <107661391+Casey1224@users.noreply.github.com>
2023-01-20 10:57:01 -0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,24 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Blockimgs from './components/Blockimgs'
 import Card from './components/Card'
-
-
+import data from './data'
+console.log(data, "data")
 export default function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        item={item}
+      />
+    )
+  })
   return (
     <div>
       <Navbar />
       <Blockimgs />
-      <Card />
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   )
 }
@@ -22,33 +32,4 @@ export default function App() {
 
 
 
-// function App() {
-//   const [count, setCount] = useState(0)
 
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src="/vite.svg" className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>
-//   )
-// }
-
-// export default App
